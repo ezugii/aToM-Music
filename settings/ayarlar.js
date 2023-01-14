f6e014def29ac5535c13bf71d773c1bbf302bf5b
PikaTube <91775058+PikaTubee@users.noreply.github.com>
2023-01-14 19:37:05 +0300

    bot

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTA2MzUwODI0MDYzOTQ2MzU2NQ.Gj2a34.FCPDSanKcvRKom8iW0-QuuxSt-0kH3P5eItaA0",  
+    PREFIX: process.env.PREFIX || "-", 
+    SAHİP: process.env.SAHİP || "772822687096897536", 
+}
