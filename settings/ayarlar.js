1f7571f0a2bf8b59a8cae755d6580977df18a0f2
aToM <91775058+atom245@users.noreply.github.com>
2023-01-15 15:35:40 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTA2MzUwODI0MDYzOTQ2MzU2NQ.Gj2a34.FCPDSanKcvRKom8iW0-QuuxSt-0kH3P5eItaA0",  
+    TOKEN: process.env.TOKEN || "MTA2MzUwODI0MDYzOTQ2MzU2NQ.GM2B__.bQP5zDlxd77DEVZlJuIJ5gQlGmoJRgVx7-bHv8",  
     PREFIX: process.env.PREFIX || "-", 
     SAHİP: process.env.SAHİP || "772822687096897536", 
 }
