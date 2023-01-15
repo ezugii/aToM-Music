c8b8aac0415674ede493a0807efc6d3f85325843
aToM <91775058+atom245@users.noreply.github.com>
2023-01-15 17:16:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const MainClient = require("./bott");
 const client = new MainClient();
-
+    client.on('ready', () => {
+     client.user.setActivity(`-oynat -yardım`)
+    });
 client.connect()
 
 module.exports = client; 
